e296dc553b3a0ebd2c15ccf9d5cb3d06b0629dbc
Aaron Brodeur <aaronb@gmail.com>
2023-09-03 06:32:30 -0700

    Clicking on board menu items closes menu

--- ui/src/BoardMenu.svelte
+++ ui/src/BoardMenu.svelte
@@ -59,7 +59,7 @@
         <h3 class="type-header">Recent Boards</h3>
         <div class="boards-section">
             {#each $uiProps.recent as boardHash }
-                <div on:click={()=>selectBoard(boardHash)}
+                <div on:click={()=>selectBoard(board.hash),()=>{store.setUIprops({showMenu:false})}}
                     class="board" id={boardHash}>{$boardList.boards.find(b=>b.hash==boardHash).name}
                 </div>
             {/each}
@@ -70,7 +70,7 @@
         <div class="boards-section">
             {#each $boardList.boards as board }
                 {#if board.status !== "archived" }
-                    <div on:click={()=>selectBoard(board.hash)}
+                    <div on:click={()=>selectBoard(board.hash),()=>{store.setUIprops({showMenu:false})}}
                     class="board" id={board.hash}>{board.name}</div>
                 {/if}
             {/each}
@@ -81,8 +81,7 @@
         <div class="boards-section">
             {#each $boardList.boards as board }
                 {#if board.status === "archived" }
-                <div class="board" id={board.hash} on:click={unarchiveBoard(board.hash)}>{board.name}</div>
-
+                <div class="board" id={board.hash} on:click={unarchiveBoard(board.hash),()=>{store.setUIprops({showMenu:false})}}>{board.name}</div>
                 {/if}
             {/each}
         </div>
