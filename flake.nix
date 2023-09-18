39077a91beb0511a127fbcb2813c92fa0ea2ee1a
Eric Harris-Braun <eric@harris-braun.com>
2023-09-18 12:51:18 -0400

    updates to run under hc-0.1.6

--- flake.nix
+++ flake.nix
@@ -1,34 +1,29 @@
 {
-  description = "Template for Holochain app development";
+  description = "Flake for Holochain app development";
 
   inputs = {
-    holochain-nix-versions.url  = "github:holochain/holochain/?dir=versions/0_2";
+    versions.url = "github:holochain/holochain?dir=versions/0_1";
     holochain-flake = {
       url = "github:holochain/holochain";
-      inputs.versions.follows = "holochain-nix-versions";
+      inputs.versions.follows = "versions";
     };
 
     nixpkgs.follows = "holochain-flake/nixpkgs";
-    flake-parts.follows = "holochain-flake/flake-parts";
   };
 
-  outputs = inputs @ { flake-parts, holochain-flake, ... }:
-    flake-parts.lib.mkFlake
-      {
-        inherit inputs;
-      }
-      {
-        systems = builtins.attrNames holochain-flake.devShells;
-        perSystem =
-          { config
-          , pkgs
-          , system
-          , ...
-          }: {
+  outputs = inputs @ { ... }:
+    inputs.holochain-flake.inputs.flake-parts.lib.mkFlake { inherit inputs; }
+    {
+        systems = builtins.attrNames inputs.holochain-flake.devShells;
+        perSystem = { config, pkgs, system, ... }: {
             devShells.default = pkgs.mkShell {
-              inputsFrom = [ holochain-flake.devShells.${system}.holonix ];
-              packages = [ pkgs.nodejs-18_x ];
+                inputsFrom = [
+                    inputs.holochain-flake.devShells.${system}.holonix
+                ];
+                packages = [
+                    pkgs.nodejs-18_x
+                ];
             };
-          };
-      };
-}   
+        };
+    };
+}
