5fc37edf8e7d9482b71905ecf9b228eca09ccd6e
Eric Harris-Braun <eric@harris-braun.com>
2023-12-06 09:43:53 -0500

    bump version in about

--- ui/src/AboutDialog.svelte
+++ ui/src/AboutDialog.svelte
@@ -58,7 +58,7 @@
 </script>
 
 
-<sl-dialog label="KanDo!: UI v0.6.0 for DNA v0.5.x" bind:this={dialog} width={600} >
+<sl-dialog label="KanDo!: UI v0.6.1 for DNA v0.5.1" bind:this={dialog} width={600} >
     <div class="about">
         <p>KanDo! is a demonstration Holochain app built by the Holochain Foundation.</p>
         <p> <b>Developers:</b>
