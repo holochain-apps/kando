5dc7e6573d71273be4f56011da95039dc3e72959
Matt Gabrenya <matt@buildyourweb.app>
2024-07-18 12:35:03 -0700

    fix(ui): type import

--- ui/src/DisableForOs.svelte
+++ ui/src/DisableForOs.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { OsType, type } from "@tauri-apps/plugin-os";
+  import { type OsType, type } from "@tauri-apps/plugin-os";
 
   export let os: OsType | OsType[] = [];
   
