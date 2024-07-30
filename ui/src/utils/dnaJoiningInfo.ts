1eedc442a6e6e333d8e46b9fe9850871f568f253
Matt Gabrenya <matt@buildyourweb.app>
2024-07-29 18:46:24 -0700

    chore(ui): remove console logs

--- ui/src/utils/dnaJoiningInfo.ts
+++ ui/src/utils/dnaJoiningInfo.ts
@@ -7,17 +7,7 @@ export interface DnaJoiningInfo {
 }
 
 // Encode a dna clone description as a base64 string for easily sharing
-export const encodeDnaJoiningInfo = (originalDnaHash: Uint8Array, name: string, networkSeed: string): string =>  {
-  console.log({originalDnaHash, name, networkSeed});
-  const val = btoa(String.fromCharCode.apply(null, encode({originalDnaHash, name, networkSeed})));
-  console.log(val);
-  console.log(encode(originalDnaHash));
-  return val;
-}
+export const encodeDnaJoiningInfo = (originalDnaHash: Uint8Array, name: string, networkSeed: string): string => btoa(String.fromCharCode.apply(null, encode({originalDnaHash, name, networkSeed})));
 
 // Decode base64 string back to clone description
-export const decodeDnaJoiningInfo = (shareCode: string): DnaJoiningInfo => {
-  const val = decode(new Uint8Array(atob(shareCode).split("").map((c) => c.charCodeAt(0)))) as DnaJoiningInfo;
-  console.log(val);
-  return val;
-}
+export const decodeDnaJoiningInfo = (shareCode: string): DnaJoiningInfo => decode(new Uint8Array(atob(shareCode).split("").map((c) => c.charCodeAt(0)))) as DnaJoiningInfo;
