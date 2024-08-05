45dc9b08c45d004e8497079681707340e09af9ea
Matt Gabrenya <matt@buildyourweb.app>
2024-08-05 10:25:07 -0700

    fix(ui): further increase zome call timeout for first launch on slow android devices

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -88,7 +88,7 @@
           await adminWebsocket.authorizeSigningCredentials(cellIds[0]);
         }
         console.log("appPort and Id is", appPort, appId);
-        const params: AppWebsocketConnectionOptions = { url: new URL(url), defaultTimeout: 120000 };
+        const params: AppWebsocketConnectionOptions = { url: new URL(url), defaultTimeout: 240000 };
         if (tokenResp) params.token = tokenResp.token;
         client = await AppWebsocket.connect(params);
       } else {
