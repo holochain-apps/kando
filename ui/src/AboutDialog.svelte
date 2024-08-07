a4ce9d209a3947666ab351caf144c338a1df5447
Matt Gabrenya <matt@buildyourweb.app>
2024-08-06 17:24:46 -0700

    feat(ui): display active network dna hash in about dialog

--- ui/src/AboutDialog.svelte
+++ ui/src/AboutDialog.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
     import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
+    import { KanDoCloneManagerStore } from './stores/cloneManager';
+    import { getContext } from 'svelte';
+    import { encodeHashToBase64 } from '@holochain/client';
 
     let dialog
     export const open = ()=>{dialog.show()}
+
+    const { getStore }: any = getContext('cloneManagerStore');
+    let cloneManagerStore: KanDoCloneManagerStore = getStore();
+  
+    $: activeDnaHash = cloneManagerStore.activeDnaHash;
+    $: activeDnaHashB64 = encodeHashToBase64($activeDnaHash);
 </script>
 
 
@@ -10,6 +19,7 @@
     <div class="about">
         <p>KanDo! is a demonstration Holochain app built by the Holochain Foundation.</p>
         <p><b>Version:</b> UI {__APP_VERSION__}; DNA {__DNA_VERSION__}</p>
+        <p><b>Active Network DNA Hash:</b> <br /><span style="font-size: 0.8rem">{activeDnaHashB64}</span></p>
         <p> <b>Developers:</b>
             Check out this hApp's source-code in our <a href="https://github.com/holochain-apps/kando">github repo</a>.
             This project's real-time syncronization is powered by <a href="https://github.com/holochain/syn">Syn</a>, 
