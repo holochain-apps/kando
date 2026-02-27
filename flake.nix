e0bfc66e7482c16dd3a6266a03771600708bdc7b
Eric Harris-Braun <eric@harris-braun.com>
2026-02-26 21:32:56 -0500

    fix: update tauri for 0.6.1

--- flake.nix
+++ flake.nix
@@ -2,40 +2,37 @@
   description = "Flake for Holochain app development";
 
   inputs = {
-    holonix.url = "github:holochain/holonix?ref=main-0.6";
+    holonix.url = "github:holochain/holonix/main-0.6";
+    p2p-shipyard.url = "github:darksoil-studio/tauri-plugin-holochain/main-0.6.1";
 
     nixpkgs.follows = "holonix/nixpkgs";
-    flake-parts.follows = "holonix/flake-parts";
+    p2p-shipyard.inputs.holonix.follows = "holonix";
   };
 
-  outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
-    systems = builtins.attrNames inputs.holonix.devShells;
-    perSystem = { inputs', pkgs, ... }: {
-      formatter = pkgs.nixpkgs-fmt;
+  outputs = inputs @ { ... }:
+    inputs.holonix.inputs.flake-parts.lib.mkFlake { inherit inputs; }
+    {
+      systems = builtins.attrNames inputs.holonix.devShells;
 
-      devShells.default = pkgs.mkShell {
-        inputsFrom = [ inputs'.holonix.devShells ];
+      perSystem =
+        { inputs', pkgs, system, ... }: {
+          devShells.default = pkgs.mkShell {
+            inputsFrom = [
+              inputs'.p2p-shipyard.devShells.holochainTauriDev
+              inputs'.holonix.devShells.default
+            ];
 
-        packages = (with inputs'.holonix.packages; [
-          holochain
-          hc
-          bootstrap-srv
-          lair-keystore
-          hc-scaffold
-          hn-introspect
-          rust # For Rust development, with the WASM target included for zome builds
-        ]) ++ (with pkgs; [
-          nodejs_22 # For UI development
-          yarn # Package manager for Node.js
-          binaryen # For WASM optimisation
-          typescript # TypeScript compiler for GraphQL adapter
-          # Add any other packages you need here
-        ]);
+            packages = with pkgs; [
+              nodejs_22
+              yarn
+              binaryen
+              typescript
+            ];
 
-        shellHook = ''
-          export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
-        '';
-      };
+            shellHook = ''
+              export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
+            '';
+          };
+        };
     };
-  };
 }
