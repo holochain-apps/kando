e19feb6de191a220e1bccfffc824f71209e17fcc
Eric Harris-Braun <eric@harris-braun.com>
2026-05-24 15:49:07 -0400

    chore: update npm deps for Holochain 0.6.1
    Bump the Holochain stack to the stable 0.6 line:
    - @holochain/client 0.20.5
    - @holochain/hc-spin 0.601.3
    - @holochain-syn/core 0.601.3
    - @holochain-open-dev/{elements,stores,utils} 0.601.1, profiles 0.601.4
    - @theweave/api 0.6.7, @theweave/elements 0.6.4
    
    Switch tests from @holochain/tryorama to @holochain-open-dev/tryorama 0.19.2
    and update the CallableCell import accordingly.

--- tests/src/kando/kando/common.ts
+++ tests/src/kando/kando/common.ts
@@ -1,2 +1,2 @@
-import { CallableCell } from '@holochain/tryorama';
+import { CallableCell } from '@holochain-open-dev/tryorama';
 import { Timestamp, NewEntryAction, ActionHash, Record, AppBundleSource, fakeActionHash, fakeAgentPubKey, fakeEntryHash, fakeDnaHash } from '@holochain/client';
